419d393ff4d139fae3b5f3d0b61478d8756f1376
Tarun kumar <tarun7011kumar@gmail.com>
2024-05-25 18:09:34 +0530

    class program

---
+++ Person.dart
@@ -0,0 +1,14 @@
+class Person {
+
+  String name;
+  int age;
+  Person(this.name, this.age);
+    void greet() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
+void main() {
+  Person obj= Person("Tarun", 22);
+  obj.greet();
+}
+
